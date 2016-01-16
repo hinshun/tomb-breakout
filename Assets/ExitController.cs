d44aea87badc8b4816cbd82744e78b9068221a6e
Edgar Lee <edgarhinshunlee@gmail.com>
2016-01-16 12:42:14 -0500

    Add winning condition

---
+++ Assets/ExitController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ExitController : MonoBehaviour {
+
+  public Text winText;
+
+  void Start() {
+    winText.text = "";
+  }
+
+  void OnTriggerEnter(Collider other) {
+    if (other.CompareTag("Player")) {
+      winText.text = "You Win!";
+    }
+	}
+}
